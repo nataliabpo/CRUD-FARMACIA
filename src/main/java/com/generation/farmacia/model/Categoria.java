a91f6eac2e7dca26979255ab893051a85b650284
nataliabpo <nataliaoliveir0108@gmail.com>
2024-01-31 15:22:20 -0300

    CRUD do Produto

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,12 +1,17 @@
 package com.generation.farmacia.model;
 
 
-import jakarta.persistence.Entity;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -25,8 +30,19 @@ public class Categoria {
 	private String nome;
 	
 	@NotNull(message = "A descrição é obrigatorio.")
-	@Size(min = 50, max = 100, message = "O atributo tipo deve conter no mínimo 05 e no máximo 100 caracteres")
+	@Size(min = 10, max = 100, message = "O atributo tipo deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy ="categoria", cascade=CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produtos> produtos;
+
+	public List<Produtos> getProdutos() {
+		return produtos;
+	}
+	public void setCategoria(List<Produtos> produtos) {
+		this.produtos = produtos;
+	}
 
 	public Long getId() {
 		return id;
---
+++ src/main/java/com/generation/farmacia/model/Produtos.java
@@ -0,0 +1,106 @@
+package com.generation.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O nome é obrigatorio.")
+	@Size(min = 2, max = 100, message = "O atributo tipo deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+
+	@NotNull(message = "O Atributo Preço é obrigatório")
+	private Double preco;
+
+	@Size(min = 5, max = 100, message = "O Atributo Indicação deve ter no mínimo 5 caracteres e no máximo 100")
+	@NotBlank(message = "Atributo Indicação é obrigatório")
+	private String indicacao;
+
+	@Size(min = 5, max = 100, message = "O Atributo Composição deve ter no mínimo 5 caracteres e no máximo 100")
+	@NotBlank(message = "Atributo Composição é obrigatório")
+	private String composicao;
+
+	@Size(min = 5, max = 100, message = "O Atributo Posologia deve ter no mínimo 5 caracteres e no máximo 100")
+	@NotBlank(message = "Atributo Posologia é obrigatório")
+	private String posologia;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Produtos produtos;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getIndicacao() {
+		return indicacao;
+	}
+
+	public void setIndicacao(String indicacao) {
+		this.indicacao = indicacao;
+	}
+
+	public String getComposicao() {
+		return composicao;
+	}
+
+	public void setComposicao(String composicao) {
+		this.composicao = composicao;
+	}
+
+	public String getPosologia() {
+		return posologia;
+	}
+
+	public void setPosologia(String posologia) {
+		this.posologia = posologia;
+	}
+
+	public Produtos getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(Produtos produtos) {
+		this.produtos = produtos;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
